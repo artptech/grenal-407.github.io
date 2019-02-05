4f8dc90203f7d42509e2ff2487ea2bc81f556e31
artptrapp@hotmail.com <artptrapp@hotmail.com>
2019-02-05 15:21:12 -0200

    Initial commit

---
+++ precache-manifest.9c4ce51a5d6aa721ee79c6f29753bcc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d51bd78a1acb153d1451b924d8430ff",
+    "url": "/static/media/sci_logo.3d51bd78.png"
+  },
+  {
+    "revision": "6d2d61d1fdd96b38eec7324578e04e6a",
+    "url": "/static/media/gremio_logo.6d2d61d1.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce13cec477f0422c2129",
+    "url": "/static/js/main.ce13cec4.chunk.js"
+  },
+  {
+    "revision": "9c0a1930f2cdf28481c8",
+    "url": "/static/js/1.9c0a1930.chunk.js"
+  },
+  {
+    "revision": "ce13cec477f0422c2129",
+    "url": "/static/css/main.1cae5298.chunk.css"
+  },
+  {
+    "revision": "6d4cccf639cffe4cc8c4fbab8c3d3160",
+    "url": "/index.html"
+  }
+];
